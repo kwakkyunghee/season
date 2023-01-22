e2300bb30fcf55fd5a57bdb2509672bd1f9dd38b
곽경희 <82927462+kwakkyunghee@users.noreply.github.com>
2023-01-22 23:23:03 +0900

    회원가입

--- season/backend/sign_up.php
+++ season/backend/sign_up.php
@@ -1,5 +1,31 @@
 <?php
+include("conn.php");
+$uid=$_POST['uid'];
+$upass=$_POST['upass'];
+$umail=$_POST['umail'];
+$uname=$_POST['uname'];
+
+$sql="select id from season_member;";
+$sel = mysqli_query($conn, $sql);
+$n=mysqli_num_rows($sel);
+
+if($uid==null) echo "<script>alert('아이디를 입력하세요.'); history.go(-1);</script>";
+else for($i=0; $i<$n; $i++){
+    $re=mysqli_fetch_row($sel); 
+    if((string)$re[0]==$uid){
+        echo "<script>aert('이미 사용중인 아이디 입니다.'); history.go(-1);</script>";break;
+    }
+}
+if($upass==null) echo "<script>alert('비밀번호를 입력해주세요.');history.go(-1);</script>";
+if($uname==null) echo "<script>alert('이름을 입력해주세요.');history.go(-1);</script>";
+//이메일도 필수항목으로 해야하나?
+
+else{
+    $sql="insert into season_member(id, pass, name, email) value('$uid', '$upass', '$uname');";
+    mysqli_query($conn, $sql);
+    echo "<script>alert('회원가입되었습니다.'); history.go(-1);</script>";
+}
 
 
-
+mysqli_close($conn);
 ?>
